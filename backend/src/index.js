75f7c4619e1a1a57d04ed9c8577daca8c9e5b88d
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-07-24 13:23:07 +0530

    index.js fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-// dotenv.config();
-dotenv.config({ quiet: true }); // surppress [dotenv@17.2.0] injecting env (2) from .env
-import cookieParser  from "cookie-parser";
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-import path from "path";
+dotenv.config();
 
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// const app = express(); socket me hai
-const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials : true,
-})) 
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -34,7 +35,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
 server.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
